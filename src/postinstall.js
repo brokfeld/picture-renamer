cccfa2774b15491eb95b45cf5ad89fe202c26ed2
Jens Brokfeld <geofreak.de@gmail.com>
2023-04-16 06:22:28 +0200

    postinstall and uninstall

---
+++ src/postinstall.js
@@ -0,0 +1,14 @@
+import fs from 'node:fs/promises';
+import os from 'node:os';
+
+(async () => {
+  const platform = os.platform();
+
+  if(platform === `linux`){
+    
+  }else{
+    
+  }
+  console.log(os.platform())
+})();
+
